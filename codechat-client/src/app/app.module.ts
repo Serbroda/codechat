5b91a889b71e6e4af87eec076c16033c44645702
Danny Rottstegge <danny@rottstegge.net>
2021-08-01 22:12:26 +0200

    Add codechat-client module

---
+++ codechat-client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { environment } from '../environments/environment';
+import { BASE_PATH } from 'codechat-sdk-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [{ provide: BASE_PATH, useValue: environment.apiBaseUrl }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
